c90d96fb6cbfc6db880f1a07458fdd6b9f976dce
Rodrigo Medeiros <rmedeiros@techsocial.com.br>
2019-10-15 08:44:16 -0300

    now refactoring to use hasura

--- src/index.js
+++ src/index.js
@@ -1,24 +1,51 @@
-const { GraphQLServer } = require("graphql-yoga");
+const { ApolloServer, gql } = require('apollo-server')
 
 const PORT = process.env.PORT || 5000;
-const schema = require("./graphql");
-const { checkUser, permissions } = require("./graphql/shield");
+// const schema = require("./graphql");
+// const { checkUser, permissions } = require("./graphql/shield");
 
-const server = new GraphQLServer({
-  schema: schema,
-  middlewares: [permissions],
-  context: req => ({
-    ...req,
-    userLogged: checkUser(req)
-  })
-});
+// const server = new GraphQLServer({
+//   schema: schema,
+//   middlewares: [permissions],
+//   context: req => ({
+//     ...req,
+//     userLogged: checkUser(req)
+//   })
+// });
 
 const serverOptions = {
   port: PORT
 };
 
-server.start(serverOptions, ({ port }) =>
-  console.log(
-    `Server started, listening on port ${port} for incoming requests.`
-  )
-);
+const { resolvers } = require('./resolvers')
+
+const { getUserId } = require('./utils')
+
+const typeDefs = gql`
+  type Query {
+    me: User!
+  }
+  type Mutation {
+    signup(email: String, password: String): AuthPayload!
+    login(email: String, password: String): AuthPayload!
+  }
+  type AuthPayload {
+    token: String
+  }
+  type User {
+    email: String
+  }
+`
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => {
+    const userId = getUserId(req)
+    return { ...req, userId }
+  }
+})
+
+server.listen(serverOptions).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`)
+})
