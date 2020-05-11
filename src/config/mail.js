92e7e005e7ba47f09781150681ab93b6da285e4e
Rodrigo Medeiros <contato@medeirostec.com.br>
2020-05-11 13:10:17 -0300

    update mail config

--- src/config/mail.js
+++ src/config/mail.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 const mail = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
-  secure: false, // true for 465, false for other ports
+  secure: true, // true for 465, false for other ports
   auth: {
     user: process.env.SMTP_USERNAME,
     pass: process.env.SMTP_PASSWORD
