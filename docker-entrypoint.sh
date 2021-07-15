5d77b828aaede37641602ad9c8bbd00ae9866b0d
Rodrigo Medeiros <contato@medeirostec.com.br>
2021-07-15 13:07:48 -0300

    adjust docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Changing Nginx default port to ${PORT}"
-/usr/bin/envsubst < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+/usr/bin/envsubst '$PORT' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 echo "Adding ENVVARS to supervisor.conf"
 /usr/bin/envsubst < /etc/supervisord.conf.template > /etc/supervisord.conf
